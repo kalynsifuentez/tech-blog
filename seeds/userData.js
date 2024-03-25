423d8fae9f52ce35d5f66e5dfdce3b0d29cae71b
kalynsifuentez <kalynsifuentez@gmail.com>
2024-03-24 23:14:22 -0500

    removed seeds emails

--- seeds/userData.js
+++ seeds/userData.js
@@ -2,27 +2,22 @@ const { User } = require("../models");
 const userData = [
   {
     username: "JohnDoe",
-    email: "JohnDoe@example.com",
     password: "pa$$word1234",
   },
   {
     username: "JaneDoe",
-    email: "JaneDoe@example.com",
     password: "Password471",
   },
   {
     username: "KateTill",
-    email: "KateTill@example.com",
     password: "PassworD4M3",
   },
   {
     username: "DougJones",
-    email: "DougJones@example.com",
     password: "MyPassword321",
   },
   {
     username: "JackWright",
-    email: "JackWright@example.com",
     password: "OpenSeseme123",
   },
 
